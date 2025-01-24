5dc77daf6b4fb542c881fc7f26d8e2c5213c86c1
llhyeon <0617mosses@gmail.com>
2025-01-24 17:15:07 +0900

    [Delete] 프로젝트 재구성을 위해 삭제

--- index.ts
+++
@@ -1,7 +0,0 @@
-import React from "./lib/react.js";
-import ReactDOM from "./lib/react-dom/client.js";
-import App from "./src/App.ts";
-
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(root).render(React.createElement(App));
