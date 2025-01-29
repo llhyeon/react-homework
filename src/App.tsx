1aa00d6123cd0c52aa5af3c2ec2e7446ec402680
llhyeon <0617mosses@gmail.com>
2025-01-29 18:49:09 +0900

    [컴포넌트] LoginForm 컴포넌트 개발
    - FormInput 컴포넌트와 Button 컴포넌트롤 사용하여 LoginForm 컴포넌트 개발

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
     <>
-      <h2>SignUp Form</h2>
+      <h2 style={{ marginBlock: "2rem" }}>SignUp Form</h2>
       <RegisterForm />
+      <hr />
+      <h2 style={{ marginBlock: "2rem" }}>SignIn Form</h2>
+      <LoginForm />
     </>
   );
 }
--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -3,13 +3,26 @@ import FormInput from "@/components/FormInput";
 import "@/components/RegisterForm.css";
 
 export default function RegisterForm() {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
   return (
-    <div className="register">
-      <FormInput label="이름" placeholder="2글자 이상 입력" />
-      <FormInput label="이메일" placeholder="user@company.id" />
-      <FormInput label="패스워드" placeholder="숫자, 영문 조합 6자리 이상 입력" buttonOn />
-      <FormInput label="패스워드 확인" placeholder="입력한 패스워드 다시 입력" buttonOn />
-      <Button>회원가입</Button>
-    </div>
+    <form className="register" onSubmit={handleSubmit}>
+      <FormInput label="이름" type="text" placeholder="2글자 이상 입력" />
+      <FormInput label="이메일" type="email" placeholder="user@company.id" />
+      <FormInput
+        label="패스워드"
+        type="password"
+        placeholder="숫자, 영문 조합 6자리 이상 입력"
+        buttonOn
+      />
+      <FormInput
+        label="패스워드 확인"
+        type="password"
+        placeholder="입력한 패스워드 다시 입력"
+        buttonOn
+      />
+      <Button type="submit">회원가입</Button>
+    </form>
   );
 }
