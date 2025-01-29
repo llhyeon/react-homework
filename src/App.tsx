ca1ede82e038226134b2030efb0b4b074862f341
llhyeon <0617mosses@gmail.com>
2025-01-29 17:06:52 +0900

    [컴포넌트] FormInput 컴포넌트
    - useState 훅을 통한 상태 관리 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import FormInput from "./components/FormInput";
 function App() {
   return (
     <>
-      <FormInput label="이름" type="text" placeholder="2글자 이상 입력" />
+      <FormInput label="이름" type="text" placeholder="2글자 이상 입력" buttonOn />
+      <FormInput label="이메일" type="email" placeholder="user@company.io" />
+      <FormInput
+        label="비밀번호"
+        type="password"
+        placeholder="숫자, 영문 조합 6자리 이상 입력"
+        buttonOn
+      />
     </>
   );
 }
--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,12 +1,18 @@
-import { ComponentProps, useId } from "react";
+import { ComponentProps, useId, useState } from "react";
 import "./FormInput.css";
 
 type FormInputProps = ComponentProps<"input"> & {
   label: string;
+  buttonOn?: boolean;
 };
 
-export default function FormInput({ label, ...restProps }: FormInputProps) {
+export default function FormInput({ label, buttonOn, ...restProps }: FormInputProps) {
   const id = useId();
+  const [inputValue, setInputValue] = useState<string>("");
+
+  function handleChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
 
   // checkbox, radio는 다른 컴포넌트로 관리할 예정, type = radio || checkbox 일 경우 반환 X
   if (restProps.type === "checkbox" || restProps.type === "radio") {
@@ -16,7 +22,15 @@ export default function FormInput({ label, ...restProps }: FormInputProps) {
   return (
     <div className="form-input">
       <label htmlFor={id}>{label}</label>
-      <input id={id} {...restProps} />
+      <div>
+        <input id={id} value={inputValue} onChange={handleChangeInput} {...restProps} />
+        {buttonOn && (
+          <button type="button">
+            <img src="/icon/hidden.svg" alt="보이기 및 숨기기" width={20} height={20} />
+          </button>
+        )}
+      </div>
+      <hr />
     </div>
   );
 }
