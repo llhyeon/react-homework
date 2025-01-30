2d315139b69aec37a4b94f9297841a957435fbe0
llhyeon <0617mosses@gmail.com>
2025-01-30 15:01:17 +0900

    [컴포넌트 및 문서화]
    - 쿠팡 Input 컴포넌트 개발 중
    - 문서화 작업 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import Input from "./components/Coopang/Input";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
     <>
-      <h2 style={{ marginBlock: "2rem" }}>SignUp Form</h2>
+      {/* <h2 style={{ marginBlock: "2rem" }}>SignUp Form</h2>
       <RegisterForm />
       <hr />
       <h2 style={{ marginBlock: "2rem" }}>SignIn Form</h2>
-      <LoginForm />
+      <LoginForm /> */}
+      <Input label="이메일" type="email" placeholder="아이디(이메일)" />
+      <Input label="비밀번호" type="password" placeholder="비밀번호" />
     </>
   );
 }
