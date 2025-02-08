8bc3a9bbaf41a13353dae682cf116ca3804a6a0e
llhyeon <0617mosses@gmail.com>
2025-02-08 21:52:34 +0900

    [과제 진행] 과제 진행 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import Input from "./components/Coopang/Input";
-import LoginForm from "./components/LoginForm";
-import RegisterForm from "./components/RegisterForm";
+import CartList from "./components/cart/cart-list";
+import CartLogo from "./components/cart/cart-logo";
+import tm from "./utils/tw-merge";
 
 function App() {
   return (
     <>
-      {/* <h2 style={{ marginBlock: "2rem" }}>SignUp Form</h2>
-      <RegisterForm />
-      <hr />
-      <h2 style={{ marginBlock: "2rem" }}>SignIn Form</h2>
-      <LoginForm /> */}
-      <Input label="이메일" type="email" placeholder="아이디(이메일)" />
-      <Input label="비밀번호" type="password" placeholder="비밀번호" />
+      <section className={tm("min-w-[320px] max-w-[800px] px-6 ")}>
+        <CartLogo />
+        <CartList />
+      </section>
     </>
   );
 }
